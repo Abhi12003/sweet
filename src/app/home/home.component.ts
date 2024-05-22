b2ecb5ef4704d65a06804d7e82d695425e3ba29b
abhi2003 <abhishekvernekar2021@gmail.com>
2024-05-22 20:59:00 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,14 +17,14 @@ export class HomeComponent {
   totalPages = Math.ceil(this.cards.length / 4);
 
   cakes: { id: number, image: string; title: string; price: number; quantity: number }[] = [
-    { id: 1, image: './assets/chocake.webp', title: 'Chocolate Cake', price: 1799, quantity: 1 },
-    { id: 2, image: './assets/bestseller/rdvcake.jpg', title: 'Red Velvet Cake', price: 1399, quantity: 1 },
+    { id: 1, image: './assets/chocake.webp', title: 'Chocolate Cake', price: 499, quantity: 1 },
+    { id: 2, image: './assets/bestseller/rdvcake.jpg', title: 'Red Velvet Cake', price: 399, quantity: 1 },
     { id: 3, image: './assets/bestseller/pinecake.jpeg', title: 'PineApple Cake', price: 999, quantity: 1 },
-    { id: 4, image: './assets/bestseller/cheesecake.webp', title: 'Cheesecake', price: 1899, quantity: 1 },
-    { id: 5, image: './assets/bestseller/vanillacake.webp', title: 'Vanilla Cake', price: 899, quantity: 1 },
-    { id: 6, image: './assets/bestseller/strawberrycake.jpg', title: 'Strawberry Cake', price: 1399, quantity: 1 },
-    { id: 7, image: './assets/bestseller/blackforestcake.webp', title: 'Black Forest Cake', price: 999, quantity: 1 },
-    { id: 8, image: './assets/bestseller/kitkatcake.jpg', title: 'KitKat Cake', price: 1299, quantity: 1 }
+    { id: 4, image: './assets/bestseller/cheesecake.webp', title: 'Cheesecake', price: 899, quantity: 1 },
+    { id: 5, image: './assets/bestseller/vanillacake.webp', title: 'Vanilla Cake', price: 799, quantity: 1 },
+    { id: 6, image: './assets/bestseller/strawberrycake.jpg', title: 'Strawberry Cake', price: 499, quantity: 1 },
+    { id: 7, image: './assets/bestseller/blackforestcake.webp', title: 'Black Forest Cake', price: 599, quantity: 1 },
+    { id: 8, image: './assets/bestseller/kitkatcake.jpg', title: 'KitKat Cake', price: 399, quantity: 1 }
   ];
 
   constructor(private cartService: CartService) {}
--- src/app/products/cakes/cakes.component.ts
+++ src/app/products/cakes/cakes.component.ts
@@ -10,8 +10,6 @@ export class CakesComponent {
   cakes: { id: number, image: string; title: string; price: number; quantity: number}[] = [
     { id: 1, image: './assets/chocake.webp', title: 'Chocolate Cake', price: 1799, quantity: 1 },
     { id: 2, image: './assets/bestseller/rdvcake.jpg', title: 'Red Velvet Cake', price: 1399, quantity: 1},
-    { id: 3, image: './assets/bestseller/pinecake.jpeg', title: 'PineApple Cake', price: 999, quantity: 1 },
-    { id: 4, image: './assets/bestseller/cheesecake.webp', title: 'Cheesecake', price: 1899, quantity: 1},
     { id: 5, image: './assets/bestseller/vanillacake.webp', title: 'Vanilla Cake', price: 899, quantity: 1},
     { id: 6, image: './assets/bestseller/strawberrycake.jpg', title: 'Strawberry Cake', price: 1399, quantity: 1 },
     { id: 7, image: './assets/bestseller/blackforestcake.webp', title: 'Black Forest Cake', price: 999, quantity: 1},
